df4a49dcd66d5c1cb6c0140c7004fcf69ee2a936
Artyom <hubabuba0144@gmail.com>
2024-02-21 23:09:56 +0300

    Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,16 @@
+﻿// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+System.Console.WriteLine("Enter the number");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num>999 || num <100)
+{
+    System.Console.WriteLine("Invalid number");
+}
+else
+{
+    System.Console.WriteLine($"{num} -> {num % 10}");
+}
