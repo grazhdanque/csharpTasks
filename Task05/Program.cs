5cc78fcad1d2730f1b9f1461b850513423fe7d72
Artyom <hubabuba0144@gmail.com>
2024-02-21 22:54:04 +0300

    Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,14 @@
+﻿// 5. Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2
+
+System.Console.WriteLine("Enter the number");
+int num = Convert.ToInt32(Console.ReadLine());
+int subnum = (-1 *num);
+while (subnum != num +1)
+{
+    System.Console.WriteLine(subnum);
+    subnum++;
+}
