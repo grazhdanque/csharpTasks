0ec7e2aec5cef65a5214462c1903e9b48b983297
Artyom <hubabuba0144@gmail.com>
2024-02-25 22:28:59 +0300

    Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = new Random().Next(10,99);
+System.Console.Write($"{num} -> ");
+int nume = num % 10;
+num = num / 10;
+if (num > nume) 
+{
+    System.Console.WriteLine(num);
+}
+else
+{
+    System.Console.WriteLine(nume);
+}
+// int maxDigit = num > nume ? num : nume;
