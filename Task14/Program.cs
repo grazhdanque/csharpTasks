e0629e76fdf4f01947f9ed3f157e3828b5df648b
Artyom <hubabuba0144@gmail.com>
2024-02-25 23:00:05 +0300

    Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+// if (num%7==0)
+// {
+//     if (num%23==0)
+//     {
+//         System.Console.WriteLine($"{num} -> да");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine($"{num} -> нет");
+//     }
+// }
+// else System.Console.WriteLine($"{num} -> нет");
+if (num%7==0 && num%23==0) System.Console.WriteLine(($"{num} -> да"));
+else System.Console.WriteLine(($"{num} -> нет"));
