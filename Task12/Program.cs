62626d07932b5f7393c19d5fe91e0207f366e26e
Artyom <hubabuba0144@gmail.com>
2024-02-25 22:49:03 +0300

    Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Введите два числа:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1%num2==0)
+{
+    System.Console.WriteLine("Кратно");
+}
+else{System.Console.WriteLine($"Не кратно, остаток {num1%num2}");}
