cca8f1e1ed5318305c95362a407a9ae45efe9730
Artyom <hubabuba0144@gmail.com>
2024-04-21 17:41:37 +0300

    Task16alp

---
+++ Task16/Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.WriteLine("input two numbers:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1==num2*num2 || num1*num1== num2) Console.WriteLine("да");
+else System.Console.WriteLine("нет");
