5b8e36784db506ef30a5b1969359303709f636cf
Artyom <hubabuba0144@gmail.com>
2024-04-21 18:14:43 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("input a number:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 999) {
+    while (num >999) 
+    {
+        num = num /10;
+    } 
+}
+if (num>99) System.Console.WriteLine(num%10);
+else System.Console.WriteLine("there is no third digit");
