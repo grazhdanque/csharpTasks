0c4753375f4dafb3be7780a9e6b115c59b6160ae
Artyom <hubabuba0144@gmail.com>
2024-04-21 18:23:07 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.WriteLine("enter the day of the week number(1-7)");
+int numDay = Convert.ToInt32(Console.ReadLine());
+if (numDay < 1 || numDay >7) System.Console.WriteLine("invalid number");
+else if (numDay<6) System.Console.WriteLine("no");
+else System.Console.WriteLine("yes");
