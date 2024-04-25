cb580d72c1f4976a137c4525d50810db3c5effdd
Artyom <hubabuba0144@gmail.com>
2024-04-25 22:49:46 +0300

    Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("input the quarter number");
+int numIn = Convert.ToInt32(Console.ReadLine());
+
+string Range(int num)
+{
+    if (num == 1) return "x>0;y>0";
+    if (num == 2) return "x<0;y>0";
+    if (num == 3) return "x<0;y<0";
+    if (num == 4) return "x>0;y<0";
+    return "invalid number";
+}
+string range = Range(numIn);
+System.Console.WriteLine(range);
