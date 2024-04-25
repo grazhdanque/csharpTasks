52aa8ac68e1631a36ba216f38e98a1f2cbe1fc9d
Artyom <hubabuba0144@gmail.com>
2024-04-26 00:03:40 +0300

    Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,19 @@
+﻿void Square(int number)
+{
+int count = 1;
+while (count != number+1) 
+{
+    System.Console.WriteLine($"{count}, {Math.Pow(count,2)}");
+    count++;
+}
+}
+System.Console.WriteLine("enter the number:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0 || num == 0) 
+{
+    System.Console.WriteLine("invalid value");
+}
+else
+{
+    Square(num);
+}
