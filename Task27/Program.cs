5fd29573f946b1571bc48861e8c74338a9bd9a17
Artyom <hubabuba0144@gmail.com>
2024-06-18 23:17:44 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,13 @@
+﻿int NumberSummary(int num)
+{
+    int caseN = 0;
+    while (num > 0)
+    {
+        caseN = caseN + (num % 10);
+        num = num / 10;
+    }
+    return caseN;
+}
+System.Console.WriteLine("Input your number:");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Sum is {NumberSummary(number)}");
