62e361c8cfd48d3fa2bee20e44750fbd85dc3f4d
Artyom <hubabuba0144@gmail.com>
2024-06-18 19:13:53 +0300

    Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,13 @@
+﻿int NumberCount(int Num)
+{
+    int count = 0;
+    while (Num != 0)
+    {
+        Num=Num/10;
+        count++;
+    }
+    return count;
+}
+System.Console.WriteLine("Input your number:");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Your number has {NumberCount(number)} digits");
