1955ec4823d356f1d1842c493ba50da84006e806
Artyom <hubabuba0144@gmail.com>
2024-06-18 18:58:44 +0300

    Task24

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -16,5 +16,10 @@
 }
 System.Console.WriteLine("Input your number:");
 int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0)
+{
+    System.Console.WriteLine("Неккоректный ввод");
+    return;
+}
 number = Summary(number);
 System.Console.WriteLine($"Result is {number}");
