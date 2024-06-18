022b1f8e2e260693b4a9d1a579c59f0ac79420dd
Artyom <hubabuba0144@gmail.com>
2024-06-18 19:19:01 +0300

    Task26

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -1,13 +1,12 @@
 ﻿int NumberCount(int Num)
 {
-    int Mun =Num;
     int count = 0;
     while (Num != 0)
     {
         Num = Num / 10;
         count++;
     }
-    return Mun == 0 ? 1 : count;
+    return count == 0 ? 1 : count;
 }
 System.Console.WriteLine("Input your number:");
 int number = Convert.ToInt32(Console.ReadLine());
