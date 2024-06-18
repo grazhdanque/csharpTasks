01daba4157c2fb1659503b22fa6f51d2696c948e
Artyom <hubabuba0144@gmail.com>
2024-06-18 19:29:07 +0300

    Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,25 @@
+﻿int Multiply(int num)
+{
+    int count = num;
+    num = 1;
+    while (count > 0)
+    {
+        checked
+        {
+            num = num * count;
+        }
+
+        count--;
+    }
+    return num;
+
+}
+System.Console.WriteLine("Input your number:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0)
+{
+    System.Console.WriteLine("Incorrect input");
+    return;
+}
+number = Multiply(number);
+System.Console.WriteLine($"Result is {number}");
